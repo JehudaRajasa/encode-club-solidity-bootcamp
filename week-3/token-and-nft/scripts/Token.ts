3cda9595799d6011ca7b53fe95cfb09f30c46aa5
Jehuda Rajasa <jehudarajasa@gmail.com>
2023-12-20 02:46:48 +0700

    Add week 1 - 6 lessons and final project

---
+++ week-3/token-and-nft/scripts/Token.ts
@@ -0,0 +1,37 @@
+import { ethers } from 'hardhat';
+import { MyToken__factory } from '../typechain-types';
+
+async function main() {
+  // Deployiing MyToken contract
+  const accounts = await ethers.getSigners();
+  const tokenContractFactory = new MyToken__factory(accounts[0]);
+  const tokenContract = await tokenContractFactory.deploy();
+  await tokenContract.waitForDeployment();
+  const tokenContractAddress = await tokenContract.getAddress();
+  console.log(`Contract deployed at ${tokenContractAddress}`);
+  const initialSupply = await tokenContract.totalSupply();
+  console.log(
+    `The initial supply of this token is ${initialSupply.toString()} decimals units`
+  );
+
+  // RBAC token mint
+  const code = await tokenContract.MINTER_ROLE();
+  const roleTx = await tokenContract.grantRole(code, accounts[2].address);
+  await roleTx.wait();
+  const mintTx = await tokenContract
+    .connect(accounts[2])
+    .mint(accounts[0].address, 2);
+  await mintTx.wait();
+  const [name, symbol, decimals, totalSupply] = await Promise.all([
+    tokenContract.name(),
+    tokenContract.symbol(),
+    tokenContract.decimals(),
+    tokenContract.totalSupply()
+  ]);
+  console.log({ name, symbol, decimals, totalSupply });
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
